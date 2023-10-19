8414bd0626c3b12a308a17bc79a80a4fa095af27
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-19 16:06:25 +0200

    Add entry_views to init file

--- tracker/views/__init__.py
+++ tracker/views/__init__.py
@@ -1,3 +1,6 @@
 from .app_views import (
     app_list, app_create, app_detail, app_edit, app_delete
 )
+from .entry_views import (
+    entry_list, entry_edit, entry_create, entry_delete, entry_detail
+)

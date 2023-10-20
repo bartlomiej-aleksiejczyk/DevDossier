71ba2733823bf06ab9e751f6167edd376dbc8f3f
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-20 04:33:10 +0200

    Rearrange endpoints

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from .views import (
     app_list, app_create, app_detail, app_edit, app_delete,
-    entry_list, entry_detail, entry_create, entry_edit, entry_delete
+    entry_list, entry_detail, entry_create, entry_edit, entry_delete,
 )
 
 urlpatterns = [
+
     path('app/', app_list, name='app_list'),
     path('app/create/', app_create, name='app_create'),
     path('app/<int:pk>/', app_detail, name='app_detail'),
@@ -13,9 +14,10 @@ urlpatterns = [
     path('app/<int:pk>/delete/', app_delete, name='app_delete'),
 
     # Entry endpoints
-    path('entry/', entry_list, name='entry_list'),
-    path('entry/create/', entry_create, name='entry_create'),
-    path('entry/<int:pk>/', entry_detail, name='entry_detail'),
-    path('entry/<int:pk>/edit/', entry_edit, name='entry_edit'),
-    path('entry/<int:pk>/delete/', entry_delete, name='entry_delete'),
+    path('app/<int:app_pk>/entry/', entry_list, name='entry_list_for_app'),
+    path('app/<int:app_pk>/entry/create/', entry_create, name='entry_create_for_app'),
+    path('app/<int:app_pk>/entry/<int:pk>/', entry_detail, name='entry_detail'),
+    path('app/<int:app_pk>/entry/<int:pk>/edit/', entry_edit, name='entry_edit'),
+    path('app/<int:app_pk>/entry/<int:pk>/delete/', entry_delete, name='entry_delete'),
+
 ]

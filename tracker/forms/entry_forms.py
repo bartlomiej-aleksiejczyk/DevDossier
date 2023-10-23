4119b9dd323dc6dc072fec1a38e5e471998e0e5f
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-23 07:52:34 +0200

    Add wip forms for entry

--- tracker/forms/entry_forms.py
+++ tracker/forms/entry_forms.py
@@ -1,9 +1,20 @@
 from django import forms
 
-from tracker.models import Entry
+from tracker.models import Entry, Tag
 
 
 class EntryForm(forms.ModelForm):
+    # tags = forms.ModelMultipleChoiceField(
+    #     queryset=Tag.objects.all(),
+    #     widget=forms.CheckboxSelectMultiple,
+    #     required=False
+    # )
+
     class Meta:
         model = Entry
-        fields = ('title', 'description', 'priority')
+        fields = ['title', 'description', 'type', 'status', 'priority', ]
+
+    # def __init__(self, *args, **kwargs):
+    #     super(EntryForm, self).__init__(*args, **kwargs)
+    #     if self.instance:
+    #         self.fields['tags'].initial = self.instance.tags.all()
